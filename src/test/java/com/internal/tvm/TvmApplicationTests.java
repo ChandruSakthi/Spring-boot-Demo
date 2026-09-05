968385e7dbbf0f638b4a09c1f8a44db64e2d6a0f
ChandruSakthi <chandrus2805@gmail.com>
2026-09-05 14:37:39 +0530

    new project

---
+++ src/test/java/com/internal/tvm/TvmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internal.tvm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TvmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
